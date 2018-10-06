f46b835cce0c826abd4760b9121f4c5487528d55
Nikolas Erkinheimo <nikolas.erkinheimo@aalto.fi>
2018-10-06 16:47:45 +0300

    new stuff

--- preprocessor.py
+++ preprocessor.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 class Preprocessor:
 
-	def __init__(self, path='data/'):
+	def __init__(self, path='data/', scale=True):
+		self.scale = scale
 		self.data_path = path
-		self.raw_data = pd.read_csv(self.data_path + 'train_data.csv', header=None)
+		self.raw_data = self.load_raw_data()
 		self.raw_data_labels = pd.read_csv(self.data_path + 'train_labels.csv', header=None)
+
+	def load_raw_data(self, scale=True):
+		data = pd.read_csv(self.data_path + 'train_data.csv', header=None).values
+		if self.scale:
+			data = (data - data.min(0)) / data.ptp(0)
+		return data
 
 	# loads all feature vectors from training data and validation data for unsupervised learning
 	# shape: (10907, 264)
@@ -15,20 +23,25 @@ class Preprocessor:
 
 	# transforms the label data to vectors, for example: 2 => (0, 1, 0, 0, 0, 0, 0, 0, 0, 0)
 	# shape: 
-	def transformed_labels(self):
-		pass
+	def transformed_labels(self, data):
+		unique_labels = np.unique(self.raw_data_labels)
+		labels = np.zeros((data.shape[0], unique_labels.shape[0]))
+		for i in range(data.shape[0]):
+			labels[i][int(data[i]) - 1] = 1
+		assert labels.shape == (data.shape[0], unique_labels.shape[0])
+		return labels
 
 	# normalises columns to [0.0, 1.0]
-	# shape: (4363, 264)
 	def normalise_data(self):
 		pass
 
 	# divides training data according to ratio for training purposes: (training_data, training_labels), (testing_data, testing_labels)
 	# shape: (ratio*4263, col), ((1 - ratio)*4363, col)
 	def divided_data(self, ratio=0.5, normalise=False):
-		raw = self.raw_data_labels.append(self.raw_data).sample(frac=1)
+		raw = np.column_stack((self.raw_data_labels, self.raw_data))
+		np.random.shuffle(raw)
 		assert raw.shape == (4363, 265)
-		num = ratio * raw_data.shape[0]
+		num = int(ratio * raw.shape[0])
 		training_data = raw[:num, 1:]
 		training_labels = raw[:num, :1]
 		testing_data = raw[num:, 1:]
--- testing.py
+++ testing.py
@@ -2,11 +2,30 @@ from preprocessor import Preprocessor
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 
-data_loader = Preprocessor()
+dl = Preprocessor(scale=False)
+x_train, y_train, x_test, y_test = dl.divided_data(ratio=0.5)
+y_train = dl.transformed_labels(y_train)
+y_test = dl.transformed_labels(y_test)
 
-x_train, y_train, x_test, y_test = data_loader.divide_data()
+opt = Adam(lr=1e-4)
 
 model = Sequential()
-model.add(Dense(64, input_dim=training_data.shape[1]))
+model.add(Dense(400, activation='sigmoid', input_dim=x_train.shape[1]))
+#model.add(Dropout(rate=0.5))
+model.add(Dense(200, activation='sigmoid'))
+#model.add(Dropout(rate=0.2))
+model.add(Dense(16))
+model.add(Dense(10, activation='sigmoid'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          epochs=200,
+          batch_size=1024)
+
+score = model.evaluate(x_test, y_test, batch_size=128)
+print("Score: ", score)

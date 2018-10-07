75516a5c44ed3e513129c433dffd2e66b9d8123a
Nikolas Erkinheimo <nikolas.erkinheimo@icloud.com>
2018-10-07 17:54:15 +0300

    Updated model and preprocessor. Slightly shady structure, needs to be refactored sometimes.

--- preprocessor.py
+++ preprocessor.py
@@ -4,20 +4,50 @@ from sklearn.preprocessing import MinMaxScaler
 
 class Preprocessor:
 
-	def __init__(self, path='data/', scale=True):
-		self.scale = scale
-		self.data_path = path
-		self.raw_data = self.load_raw_data()
+	def __init__(self, path='data/', balance=True, mutation_rate=0.003):
+		self.balance 		 = balance
+		self.mutation_rate 	 = mutation_rate
+		self.data_path 		 = path
 		self.raw_data_labels = pd.read_csv(self.data_path + 'train_labels.csv', header=None)
+		self.unique_labels	 = np.unique(self.raw_data_labels)
+		self.raw_data 		 = self.load_raw_data()
 
-	def load_raw_data(self, scale=True):
+	def load_raw_data(self):
 		data = pd.read_csv(self.data_path + 'train_data.csv', header=None).values
-		if self.scale:
-			data = (data - data.min(0)) / data.ptp(0)
 		return data
 
+	def balance_raw_data(self, data, labels):
+		distribution = {}
+		raw = np.hstack((labels, data))
+		for label in self.unique_labels:
+			distribution[int(label)] = 0
+		for label in labels:
+			distribution[int(label)] += 1
+		distmax = distribution[max(distribution, key=distribution.get)]
+		count = 0
+		tmp = np.empty((0, 265))
+		for label in self.unique_labels:
+			auxiliary_rows = raw[raw[:, 0] == label]
+			for aux in range(distmax - distribution[label]):
+				to_add = auxiliary_rows[np.random.randint(auxiliary_rows.shape[0])]
+				assert to_add.shape == (265, )
+				count += 1
+				if count % 1000 == 0:
+					print("Processed count: ", count)
+				tmp = np.append(tmp, [to_add], axis=0)
+		raw = np.vstack((raw, tmp))
+		new_labels = raw[:, :1]
+		new_features = raw[:, 1:]
+		print("Label shape: ", new_labels.shape, ", feature shape: ", new_features.shape)
+		# self.raw_data_labels = new_labels
+		return new_features, new_labels
+
+	def label_filter(self, row, label: int):
+		#print(int(row[0])
+		return row[0] == label
+
 	# loads all feature vectors from training data and validation data for unsupervised learning
-	# shape: (10907, 264)
+	# shape: (num_of_vectors, 264)
 	def all_feature_vectors(self):
 		pass
 
@@ -37,13 +67,14 @@ class Preprocessor:
 
 	# divides training data according to ratio for training purposes: (training_data, training_labels), (testing_data, testing_labels)
 	# shape: (ratio*4263, col), ((1 - ratio)*4363, col)
-	def divided_data(self, ratio=0.5, normalise=False):
-		raw = np.column_stack((self.raw_data_labels, self.raw_data))
+	def divided_data(self, ratio=0.5):
+		raw = np.hstack((self.raw_data_labels, self.raw_data))
 		np.random.shuffle(raw)
-		assert raw.shape == (4363, 265)
 		num = int(ratio * raw.shape[0])
 		training_data = raw[:num, 1:]
 		training_labels = raw[:num, :1]
+		if self.balance:
+			training_data, training_labels = self.balance_raw_data(training_data, training_labels)
 		testing_data = raw[num:, 1:]
 		testing_labels = raw[num:, :1]
 		return training_data, training_labels, testing_data, testing_labels
--- testing.py
+++ testing.py
@@ -1,31 +1,57 @@
 from preprocessor import Preprocessor
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Dense, Dropout, Activation, normalization
 from keras.optimizers import SGD, Adam
+from keras.regularizers import l1_l2
 
-dl = Preprocessor(scale=False)
-x_train, y_train, x_test, y_test = dl.divided_data(ratio=0.5)
+dl = Preprocessor(balance=True)
+x_train, y_train, x_test, y_test = dl.divided_data(ratio=0.8)
 y_train = dl.transformed_labels(y_train)
 y_test = dl.transformed_labels(y_test)
+use_tensorboard = True # raskas, käytä vain tarvittaessa
 
-opt = Adam(lr=1e-4)
+opt = Adam(lr=1)
 
 model = Sequential()
-model.add(Dense(400, activation='sigmoid', input_dim=x_train.shape[1]))
-#model.add(Dropout(rate=0.5))
-model.add(Dense(200, activation='sigmoid'))
-#model.add(Dropout(rate=0.2))
-model.add(Dense(16))
+model.add(normalization.BatchNormalization())
+model.add(Dense(400, 
+                activation='sigmoid', 
+                input_dim=264,
+                kernel_regularizer=l1_l2(1e-4, 1e-4)))
+model.add(Dropout(rate=0.4))
+model.add(Dense(300, 
+                activation='sigmoid'))
+model.add(Dropout(rate=0.2))
+model.add(Dense(200, 
+                activation='sigmoid'))
+model.add(Dense(150, 
+                activation='sigmoid'))
+model.add(Dense(100, 
+                activation='sigmoid'))
+model.add(Dense(60, activation='sigmoid'))
 model.add(Dense(10, activation='sigmoid'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
+              optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          epochs=200,
-          batch_size=1024)
+# toisella komentorivillä samassa hakemistossa: tensorboard --logdir ./Graph
+tensorboardCB = keras.callbacks.TensorBoard(log_dir='./Graph', 
+                                          histogram_freq=1,  
+                                          write_graph=True, 
+                                          write_images=True)
+
+if use_tensorboard:
+    model.fit(x_train, y_train,
+            epochs=1000,
+            batch_size=8192,
+            validation_data=(x_test, y_test),
+            callbacks=[tensorboardCB])
+else:
+    model.fit(x_train, y_train,
+            epochs=500,
+            batch_size=8192)
 
 score = model.evaluate(x_test, y_test, batch_size=128)
 print("Score: ", score)

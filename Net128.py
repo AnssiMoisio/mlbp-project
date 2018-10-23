a0d74673a42af485851061f4c5ffc78ef96bdd28
Nikolas Erkinheimo <nikolas.erkinheimo@aalto.fi>
2018-10-23 13:59:31 +0300

    Replaced LSTM with a normal feedforward net.

--- Net128.py
+++ Net128.py
@@ -6,48 +6,43 @@ from keras.optimizers import Adam, SGD
 from keras.regularizers import l1_l2
 import numpy as np
 
-dl = Preprocessor(balance=True, scale=True, mutation_rate=0.15)
+dl = Preprocessor(balance=False, scale=True, mutation_rate=0.15)
 x_train, y_train, x_test, y_test = dl.divided_data(ratio=0.8, load_bal_data=False)
 y_train = dl.transformed_labels(y_train)
 y_test = dl.transformed_labels(y_test)
 
-rhythm_input = Input(shape=(7, 24)) # timesteps, timestep dimension
-chroma_input = Input(shape=(4, 12)) # timesteps, timestep dimension
-mfcc_input 	 = Input(shape=(4, 12)) # timesteps, timestep dimension
+rhythm_input = Input(shape=(168,)) # timesteps, timestep dimension
+chroma_input = Input(shape=(48,)) # timesteps, timestep dimension
+mfcc_input 	 = Input(shape=(48,)) # timesteps, timestep dimension
 
-rhythm_lstm  = LSTM(48, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-7))(rhythm_input)
-rhythm_lstm  = Dropout(rate=0.25)(rhythm_lstm)
-rhythm_lstm  = Dense(48, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-5))(rhythm_lstm)
+rhythm_lstm  = Dense(168, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-6))(rhythm_input)
 rhythm_lstm  = Dropout(rate=0.5)(rhythm_lstm)
+rhythm_lstm  = Dense(50, activation='tanh', kernel_regularizer=l1_l2(1e-6, 2e-6))(rhythm_lstm)
 
-chroma_lstm  = LSTM(24, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-7))(chroma_input)
-chroma_lstm  = Dropout(rate=0.25)(chroma_lstm)
-chroma_lstm  = Dense(24, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-5))(chroma_lstm)
+chroma_lstm  = Dense(48, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-6))(chroma_input)
 chroma_lstm  = Dropout(rate=0.5)(chroma_lstm)
 
-mfcc_lstm 	 = LSTM(24, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-7))(mfcc_input)
-mfcc_lstm    = Dropout(rate=0.25)(mfcc_lstm)
-mfcc_lstm  	 = Dense(24, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-5))(mfcc_lstm)
+mfcc_lstm  	 = Dense(48, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-6))(mfcc_input)
 mfcc_lstm    = Dropout(rate=0.5)(mfcc_lstm)
 
 classifier 	 = concatenate([rhythm_lstm, chroma_lstm, mfcc_lstm], axis=-1)
 
 for rate in range(3):
-	classifier = Dense(72, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-5))(classifier)
+	classifier = Dense(100, activation='tanh', kernel_regularizer=l1_l2(1e-6, 1e-6))(classifier)
 	classifier = Dropout(rate=0.5)(classifier)
 
 classifier = Dense(10, activation='sigmoid', kernel_regularizer=l1_l2(1e-7, 1e-6))(classifier)
 
 input_data = [
-	np.reshape(x_train[:, :168], (x_train.shape[0], 7, 24)),
-	np.reshape(x_train[:, 168:216], (x_train.shape[0], 4, 12)),
-	np.reshape(x_train[:, 216:], (x_train.shape[0], 4, 12))
+	x_train[:, :168],
+	x_train[:, 168:216],
+	x_train[:, 216:]
 ]
 
 validation_data = [
-	np.reshape(x_test[:, :168], (x_test.shape[0], 7, 24)),
-	np.reshape(x_test[:, 168:216], (x_test.shape[0], 4, 12)),
-	np.reshape(x_test[:, 216:], (x_test.shape[0], 4, 12))
+	x_test[:, :168],
+	x_test[:, 168:216],
+	x_test[:, 216:]
 ]
 
 tensorboardCB = keras.callbacks.TensorBoard(log_dir='./Graph', 
@@ -64,4 +59,4 @@ model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accurac
 model.fit(input_data, y_train, 
 				validation_data=(validation_data, y_test), 
 				batch_size=256, 
-				epochs=200)
+				epochs=300)
---
+++ drafts/PCA.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+data = pd.read_csv('../data/train_data.csv')
